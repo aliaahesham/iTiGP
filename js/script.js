5969954daf1c3af818c5a5eaf7c784d80e990374
NouranFayez <nouran_ahmed269@hotmail.com>
2019-05-25 00:47:05 +0200

    car Accessories page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var owl = $('.owl-carousel');
+            owl.owlCarousel({
+                items:4,
+                loop:true,
+                margin:20,
+                autoplay:true,
+                autoplayTimeout:20000,
+                autoplayHoverPause:true
+            });
