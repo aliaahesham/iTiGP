11c0e5256c8bad7ce549af5ab7aa379f9acbd81b
lenovo@DESKTOP-5G08LJF <reememad579@gmail.com>
2019-06-03 16:40:41 +0200

    get updates

--- dealer-app/src/app/features/spare-parts/details/details.component.ts
+++ dealer-app/src/app/features/spare-parts/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-details',
+  selector: 'app-sparePart-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
