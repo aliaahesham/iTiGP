928d9c9a7efdf6f071a84b6b11ca3192907223ef
lenovo@DESKTOP-5G08LJF <reememad579@gmail.com>
2019-06-03 23:41:17 +0200

    add header dropdown style

--- dealer-app/src/app/routing.module.ts
+++ dealer-app/src/app/routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {HomepageComponent} from './core/home/homepage/homepage.component';
+import { HomepageComponent } from './core/home/homepage/homepage.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule.forRoot([
-      {path: '' , component: HomepageComponent},
-      {path:'home', redirectTo:'' ,pathMatch:'full'},
+      { path: '', component: HomepageComponent },
+      { path: 'home', redirectTo: '', pathMatch: 'full' },
       // {path:'**', component:NotFoundPAgeComponent },
 
     ])
