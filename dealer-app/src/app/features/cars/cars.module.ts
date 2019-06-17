aa021a2cc0db5ff660bcb4e7d85b279c91692a91
= <=>
2019-06-17 17:15:27 +0200

    modifed carDetails component

--- dealer-app/src/app/features/cars/cars.module.ts
+++ dealer-app/src/app/features/cars/cars.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { AddComponent } from './add/add.component';
 import { DetailsComponent } from './details/details.component';
 import { carService } from './car.services';
+import { TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [ListComponent, AddComponent, DetailsComponent],
   imports: [
     CommonModule,
+    TabsModule.forRoot(),
     RouterModule.forChild([
       { path: 'car', component: ListComponent },
       { path: 'car/add', component: AddComponent },
