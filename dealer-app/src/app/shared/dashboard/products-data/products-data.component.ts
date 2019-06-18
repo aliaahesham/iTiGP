e606bc9466fc555c0547e5a001ffb9a62780347d
lenovo@DESKTOP-5G08LJF <reememad579@gmail.com>
2019-06-18 16:18:42 +0200

    add features to dashboard

--- dealer-app/src/app/shared/dashboard/products-data/products-data.component.ts
+++ dealer-app/src/app/shared/dashboard/products-data/products-data.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
 import { Seller } from 'src/app/_models/seller';
 import { car } from 'src/app/_models/car/car';
@@ -17,13 +18,31 @@ export class ProductsDataComponent implements OnInit {
   cars: car[];
   accessories: Accessories[];
   spareParts: SpareParts[];
-  constructor(private CarService: carService, private AccessoriesService: AccessoriesService, private SparePartsService: SparePartsService) { }
+
+  constructor(private CarService: carService, private AccessoriesService: AccessoriesService, private SparePartsService: SparePartsService, private router: Router) { }
 
   ngOnInit() {
     this.accessories = this.AccessoriesService.getBySeller(this.seller.name);
     this.cars = this.CarService.getBySeller(this.seller.name);
-    console.log(this.accessories)
-    console.log(this.cars)
-  }
 
+  }
+  DeleteProduct(service, id) {
+
+    if (service === 'car') {
+      this.CarService.delete(id);
+      this.ngOnInit();
+    }
+    else if (service === 'sparePart') {
+      // this.SparePartsService.delete(id);
+      // this.ngOnInit();
+    }
+    else {
+      this.AccessoriesService.delete(id);
+      this.ngOnInit();
+    }
+  }
+  EditProduct(product) {
+    this.router.navigateByUrl(`${product}/add`);
+
+  }
 }
