b0e79adc2300cd4ce107106a25bd49771a105906
aliaahesham <aliaabaharia@outlook.com>
2019-06-25 11:49:38 +0200

    implemented spareParts listing

--- dealer-app/src/app/features/spare-parts/list/list.component.ts
+++ dealer-app/src/app/features/spare-parts/list/list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { making } from '../../../_models/car/making';
+import { makingService } from '../../cars/making.service';
+import { model } from '../../../_models/car/model';
+import { modelService } from '../../cars/model.service';
+import { category } from '../../../_models/spare-parts/category';
+import { CategoryService } from '../../spare-parts/category.service';
+import { SparePart } from '../../../_models/spare-parts/spare-parts';
+import { SparePartsService } from '../../spare-parts/spare-parts.service';
 
 @Component({
   selector: 'app-spare-parts',
@@ -6,10 +14,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  makings: making[];
+  models: model[];
+  categories: category[];
+  @Input() allSpareParts: SparePart[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private makingService: makingService,
+    private modelService: modelService,
+    private categoryService: CategoryService,
+    private sparePartsService: SparePartsService
+  ) {
+    this.makings = this.makingService.getAll();
+    this.categories = this.categoryService.getAll();
   }
 
+  ngOnInit() {
+    if (!this.allSpareParts) {
+      this.allSpareParts = this.sparePartsService.getAll();
+    }
+  }
+
+  makingOnChange(event: Event) {
+    const eventTarget = event.target as HTMLTextAreaElement;
+    const makingIdUnknown = eventTarget.value as unknown;
+    const makingId = makingIdUnknown as number;
+    this.models = this.modelService.getBymakingId(makingId);
+  }
 }
